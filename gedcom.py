9e98b4bf26f1085d672b4a7302e735051b832b7b
Gosin <canling0@gmail.com>
2014-11-07 12:38:46 -0500

    refactored gatherInfo function

--- gedcom.py
+++ gedcom.py
@@ -17,7 +17,7 @@ families = dict()
 
 extract_re = '([0-9]*) (@?\w+@?)\s*([^\n\r]*)'
 
-def gatherInfo(input_file):
+def gatherInfo(input_file, individuals, families):
 
     # tag category: 0 for indi, 1 for fam, 2 for others
     tagCat = 2
@@ -34,74 +34,100 @@ def gatherInfo(input_file):
             part3 = parts[2]
 
             if part1 == '0':
-                if part3 == "INDI":
+                if isinstance(createClass(part2, part3, individuals, families), Individual):
                     tagCat = 0
                     tempID = int(re.sub(r'@+|I+', "", part2))
-                    individuals[tempID] = Individual()
-                    individuals[tempID].addID(part2)
-                elif part3 == 'FAM':
+                elif isinstance(createClass(part2, part3, individuals, families), Family):
                     tagCat = 1
                     tempFamID = int(re.sub(r'@+|F+', "", part2))
-                    families[tempFamID] = Family()
-                    families[tempFamID].addFamID(part2)
                 else:
                     tagCat = 2
 
             elif part1 == '1':
                 if tagCat == 0:
-                    if part2 == "BIRT" or part2 == "DEAT":
-                        prevTag = part2
-                    elif part2 == 'NAME':
-                        re_name = re.sub(r'/+', "", part3)
-                        individuals[tempID].addName(re_name)
-                    elif part2 == 'SEX':
-                        individuals[tempID].addSex(part3)
-                    elif part2 == 'FAMC':
-                        individuals[tempID].addFamc(part3)
-                    elif part2 == 'FAMS':
-                        individuals[tempID].addFams(part3)
-                    else:
-                        pass
-
+                    prevTag = addIndiInfo(part2, part3, tempID, individuals)
                 elif tagCat == 1:
-                    if part2 == "MARR" or part2 == "DIV":
-                        prevTag = part2
-                    elif part2 == 'HUSB':
-                        families[tempFamID].addHusb(part3)
-                    elif part2 == 'WIFE':
-                        families[tempFamID].addWife(part3)
-                    elif part2 == "CHIL":
-                        families[tempFamID].addChil(part3)
-                    else:
-                        pass
-
+                    prevTag = addFamInfo(part2, part3, tempFamID, families)
                 elif tagCat == 2:
                     pass
-
             elif part1 == '2' and part2 == 'DATE':
                 if tagCat == 0:
-                    if prevTag == 'BIRT':
-                        individuals[tempID].addBirt(part3)
-                    elif prevTag == 'DEAT':
-                        individuals[tempID].addDeat(part3)
-                    else:
-                        pass
-
+                    addIndiDate(prevTag, tempID, individuals, part3)
                 elif tagCat == 1:
-                    if prevTag == 'MARR':
-                        families[tempFamID].addMarr(part3)
-                    elif prevTag == 'DIV':
-                        families[tempFamID].addDiv(part3)
-                    else:
-                        pass
+                    addFamDate(prevTag, tempFamID, families, part3)
+        else:
+            pass
 
-                else:
-                    pass
+def createClass(part2, part3, individuals, families):
+    if part3 == "INDI":
+        tempID = int(re.sub(r'@+|I+', "", part2))
+        individuals[tempID] = Individual()
+        individuals[tempID].addID(part2)
+        return individuals[tempID]
+    elif part3 == "FAM":
+        tempFamID = int(re.sub(r'@+|F+', "", part2))
+        families[tempFamID] = Family()
+        families[tempFamID].addFamID(part2)
+        return families[tempFamID]
+    else:
+        return None
+
+def addIndiInfo(part2, part3, tempID, individuals):
+    if part2 == "BIRT" or part2 == "DEAT":
+        return part2
+    elif part2 == "NAME":
+        re_name = re.sub(r'/+', "", part3)
+        individuals[tempID].addName(re_name)
+        return None
+    elif part2 == "SEX":
+        individuals[tempID].addSex(part3)
+        return None
+    elif part2 == "FAMC":
+        individuals[tempID].addFamc(part3)
+        return None
+    elif part2 == "FAMS":
+        individuals[tempID].addFams(part3)
+        return None
+    else:
+        return None
+
+def addFamInfo(part2, part3, tempFamID, families):
+    if part2 == "MARR" or part2 == "DIV":
+        return part2
+    elif part2 == "HUSB":
+        families[tempFamID].addHusb(part3)
+        return None
+    elif part2 == "WIFE":
+        families[tempFamID].addWife(part3)
+        return None
+    elif part2 == "CHIL":
+        families[tempFamID].addChil(part3)
+        return None
+    else:
+        return None
+
+def addIndiDate(prevTag, tempID, individuals, part3):
+    if prevTag == "BIRT":
+        individuals[tempID].addBirt(part3)
+    elif prevTag == "DEAT":
+        individuals[tempID].addDeat(part3)
+    else:
+        pass
+
+def addFamDate(prevTag, tempFamID, families, part3):
+    if prevTag == "MARR":
+        families[tempFamID].addMarr(part3)
+    elif prevTag == "DIV":
+        families[tempFamID].addDiv(part3)
+    else:
+        pass
+
+
 
 
 def main(arg1):
     input_file = open(arg1, 'r')
-    gatherInfo(input_file)
+    gatherInfo(input_file, individuals, families)
     outputIndiSummary(individuals)
     print 
     outputFamSummary(families, individuals)
